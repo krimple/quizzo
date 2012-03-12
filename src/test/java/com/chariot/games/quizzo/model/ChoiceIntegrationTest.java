ac73d09ce9ebe629630b7b05d530d45a43ba8901
Ken Rimple <krimple@chariotsolutions.com>
2012-03-12 14:02:26 -0400

    updates for dojo 1.7 in progress

--- src/test/java/com/chariot/games/quizzo/model/ChoiceIntegrationTest.java
+++ src/test/java/com/chariot/games/quizzo/model/ChoiceIntegrationTest.java
@@ -3,7 +3,7 @@ package com.chariot.games.quizzo.model;
 import org.junit.Test;
 import org.springframework.roo.addon.test.RooIntegrationTest;
 
-@RooIntegrationTest(entity = Choice.class)
+@RooIntegrationTest(entity = Choice.class, findAllMaximum = 400)
 public class ChoiceIntegrationTest {
 
     @Test
